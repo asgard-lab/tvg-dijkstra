e873127a2948615f14cdfa2063c52fef95bac36b
ÁLVARO SHIOKAWA ALVAREZ <alvaro.shiokawa@gmail.com>
2015-11-21 18:58:38 -0200

    tvsp completo só mudei uns termos pra ingles
    versao final

---
+++ one/tvg/TVG.java
@@ -0,0 +1,174 @@
+package tvg;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class TVG {
+	/**==================**/
+	/**= Singleton Part =**/
+	/**==================**/
+	/* We decided that for our TheONE project a single instance of the graph will remain active in the whole simulation! */
+	private static TVG singleton_tvg;
+	
+	public synchronized static TVG getInstance(){
+		if (singleton_tvg == null)
+			singleton_tvg = new TVG();
+		return singleton_tvg;
+	}
+	
+	/**==============**/
+	/**= Attributes =**/
+	/**==============**/
+	List<Node> nodes;
+	List<Edge> edges;
+	LinkedList<Node> fastest_path;
+	
+	/**===========**/
+	/**= Methods =**/
+	/**===========**/
+	/* Default Constructor */
+	public TVG(){
+		nodes = new ArrayList<Node>(); // List where them nodes will be stored!
+		edges = new ArrayList<Edge>(); // List where them edges will be stored!
+	}	
+	
+	/* Specific Constructor */
+	public TVG(List<Node> nodes, List<Edge> edges) {
+		this.nodes = nodes;
+		this.edges = edges;
+	}
+	
+	/* getNodes */
+	public List<Node> getNodes() {
+		return nodes; // Return the list of the nodes stored in the graph!
+	}
+	
+	/* getNodeByLabel */
+	// Search for an specific node, by its label, inside a specific node list!
+	public Node getNodeByLabel(String label_parametro, List<Node> list){
+		for(Node n : list)
+			if(n.getLabel().equals(label_parametro))
+				return n;
+		return null;
+	}
+	
+	/* getEdges */
+	public List<Edge> getEdges() {
+		return edges; // Returns the list of the edges stored in the graph!
+	}
+	
+	/* getEdgeByLabel */
+	// Search for a specific edge, by its label, inside a specific edge list!
+	public Edge getEdgeByLabel(String label_parametro, List<Edge> list){
+		for(Edge n : list)
+			if(n.getLabel().equals(label_parametro))
+				return n;
+		return null;
+	}
+
+	/* isInsideAnInInterval */
+	// Given a certain time, during simulation, verify if such time is part of a given edge (Such time may be in any of the edge's intervals)
+	public boolean isInsideAnInterval(double t, Edge a){
+		for (EdgeInterval in : a.getIntervals()){
+			if(t>=in.getStartTime() && t<=in.getEndTime())
+				return true;
+		}
+		return false;
+	}	
+	
+	/* setShortestPath */
+	public void setShortestPath(LinkedList<Node> caminho) {
+		fastest_path = caminho;
+	}
+
+	/* getCurrentShortestPath */
+	public LinkedList<Node> getCurrentShortestPath() {
+		return fastest_path;
+	}
+	
+	/* populateGraph */
+	// Auxiliary method we call only once in order to populate the TVG accordingly (With nodes and edges), after reading a dot file.
+	public void populateGraph(){
+		Pattern pattern_edge = Pattern.compile("[a-zA-Z][0-9]+--[a-zA-Z][0-9]+");
+		Pattern pattern_intervals = Pattern.compile("[0-9]+.[0-9]+,[0-9]+.[0-9]+");
+        Matcher matcher; // Vari�vel auxiliar que faz o casamento da string lida com o padr�o procurado.
+		String[] aux_array = null; // Array de string auxiliar para a execu��o de splits e populamento das estruturas de dados do TVG.
+		String aux;
+		Node new_node1 = null;
+		Node new_node2 = null;
+		Edge new_edge = null;
+		double new_start_time = 0.0;
+		double new_end_time = 0.0;
+		
+		
+        try {
+        	// Tenta abrir o arquivo do TVG para leitura
+        	File file =  new File("tvg"+File.separator+"TVG.dot");
+        	RandomAccessFile file_reader = new RandomAccessFile(file, "r");
+        	
+        	// Primeira leitura do arquivo é pra popular a node array list.
+        	file_reader.seek(0); // Volta o ponteiro de leitura para o começo do arquivo.
+        	String current_line = file_reader.readLine(); // Lê a primeira linha "graph {"
+        	while (current_line != null){
+        		matcher = pattern_edge.matcher(current_line);
+        		if (matcher.find()){
+        			aux = matcher.group(); // acredito que tenha q usar este group() dentro do matcher.find()
+        			aux_array = aux.split("--"); // Isola os dois nós da aresta.
+        			
+        			/* Adiciona cada um dos nodes da aresta lidos ao array list de nodes do TVG */
+        			new_node1 = new Node(aux_array[0],aux_array[0]);
+        			new_node2 = new Node(aux_array[1],aux_array[1]);
+        			if (!nodes.contains(new_node1)) // Se a lista de nós não contiver um nó igual ao temp_node1, adicione!
+        				nodes.add(new_node1); // Adiciona um nó da aresta à lista de nós!
+        			
+        			if (!nodes.contains(new_node2)) // Se a lista de nós não contiver um nó igual ao temp_node1, adicione!
+        				nodes.add(new_node2); // Adiciona o outro nó da aresta à lista de nós!        			
+        		}
+        		current_line = file_reader.readLine(); // Avança ponteiro de leitura do arquivo pra próxima linha.
+        	}
+    		
+    		
+    		// Segunda leitura do arquivo, para adicionar arestas e seus respectivos intervalos
+        	file_reader.seek(0); // Volta o ponteiro de leitura para o começo do arquivo.
+        	current_line = file_reader.readLine(); // Lê a primeira linha "graph {"
+        	while (current_line != null){
+        		matcher = pattern_edge.matcher(current_line);
+        		if (matcher.find()){
+        			aux = matcher.group(); // acredito que tenha q usar este group() dentro do matcher.find()
+        			aux_array = aux.split("--"); // Isola os dois nós da aresta.
+        			
+        			/* Procura os nodes na array list nodes */
+					new_node1 = getNodeByLabel(aux_array[0],nodes);
+					new_node2 = getNodeByLabel(aux_array[1],nodes);
+					
+    				matcher = pattern_intervals.matcher(current_line); // Procura por intervalos
+    				while (matcher.find()){
+    					new_edge = new Edge(new_node1.getLabel()+"--"+new_node2.getLabel(),new_node1,new_node2,0.0);
+    					aux = matcher.group();
+    					aux_array = aux.split(",");
+    					new_start_time = Double.parseDouble(aux_array[0]);
+    					new_end_time = Double.parseDouble(aux_array[1]);
+    					new_edge.addInterval(new_start_time,new_end_time);
+        				new_edge.setWeight(new_start_time);
+    					//new_edge.setWeight(new_edge.getMinimumIntervalStartTime());
+        				edges.add(new_edge);
+    				}
+					
+				}
+        		current_line = file_reader.readLine(); // Avança ponteiro de leitura do arquivo pra próxima linha.
+        	}
+    		file_reader.close(); // Sempre que abre um arquivo, é bom fechá-lo.
+        } catch (IOException e) {
+        	System.out.println("Arquivo inexistente");
+        }
+	}
+}
---
+++ one/tvg/TVSP_Algorithm.java
@@ -0,0 +1,250 @@
+package tvg;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import core.DTNHost;
+/* NOTE: This algorithm is heavily based off Lars Vogel's Djikstra Algorithm implementation, so credits to him for the original code!
+ * The link to his implementation is at http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html */
+public class TVSP_Algorithm {
+	/**==============**/
+	/**= Attributes =**/
+	/**==============**/
+	List<Node> nodes;
+	List<Edge> edges;
+	List<Edge> filtered_edges;
+	Set<Node> visitedNodes;
+	Set<Node> unvisitedNodes;
+	Map<Node, Node> predecessors;
+	Map<Node, Node> alternate_predecessors;
+	Map<Node, Double> accumulated_time; // HashMap which setsa node as a key, and its accumulated weight, distance on the node.
+	Double current_interval_time;
+	
+	/**===========**/
+	/**= Métodos =**/
+	/**===========**/
+	/* Specific Constructor */
+	public TVSP_Algorithm(TVG tvg) {
+		// Copies the graph's nodes and edges into the Djikstra object so that one can operate on both.
+		this.nodes = new ArrayList<Node>(tvg.getNodes());
+		this.edges = new ArrayList<Edge>(tvg.getEdges());
+		this.filtered_edges = new ArrayList<Edge>();
+	}
+	
+	/* Execute (Call this method to start running TVSP, just input a source node and hope for the best!) */
+	public void execute_tvsp(Node source) {
+		visitedNodes = new HashSet<Node>();
+		unvisitedNodes = new HashSet<Node>();
+		accumulated_time = new HashMap<Node, Double>(); // HashMap which sets a node as a key, and its accumulated time on the node.
+		predecessors = new HashMap<Node, Node>(); // Hash map containing a node and the predecessor node to it. Useful to map the shortet path.
+		alternate_predecessors = new HashMap<Node, Node>();
+		
+		// Initially only source node is in unsettled nodes set, and has distance 0.
+		accumulated_time.put(source, 0.0); // Returns the accumulated weight of a node (Or accumulated distance of node). In this case we initially set origin node with weight 0.
+		unvisitedNodes.add(source); // Initially only the source node is in this set of unvisited nodes.
+		while (unvisitedNodes.size() > 0) {
+			/*
+			 * At firt only the source node is inside the unvisitedNodes set, so after analyizing it, it is now part of the visitednodes
+			 * */
+			
+			Node node = getMinimumTimeNode(unvisitedNodes); // Returns unvisited neighbouring node of least accumulated weight
+			visitedNodes.add(node); // Adds the least accumulated weight node for evaluation.
+			unvisitedNodes.remove(node); // Since it was already visited, and is currently being evaluated, it is not unvisited anymore.
+			relaxationProcess(node); // Here we find the proper neighbours of the node with the least weight.
+		}
+	}
+
+	/*
+	 * HERE IN THIS METHOD THAT THE RELAXATION PROCESS WILL HAPPEn (RELAXATION similar to djikstra)
+	 * */
+	public void relaxationProcess(Node node) {
+		List<Node> adjacentNodes = getReachableNeighbors(node);
+		for (Node target : adjacentNodes) {
+			/** This part is derived from the famous relaxation process in Djikstra**/
+			
+			/*
+			 * The mcgyverism here in order to make this thing work is the following:
+			 * We consider that the cost to traverse an edge, that is, the tranmission time between nodes,
+			 * is practially always the same (And indeed it is, because according to our simultion logs, such transmission
+			 * time is almost always the same, for all nodes).
+			 * 
+			 * In similar terms to Djikstra, the getShortestDistance method returns the accumulated weight from a node, and the
+			 * getDistance(node, target) method returns the edge weight, which, in our implementation, is the lower bound of the
+			 * time interval within the edge, meaning it refers to the starting time of the interval
+			 * 
+			 * Initially, source node's weight is 0, and every other node has INFINITY as their weight.
+			 * During this relaxation phase, we verify if the destination node's weight (In other words, a neighbour node) is bigger than
+			 * the distance it takes to reach it. If it is bigger, we set this target node's weight with a smaller value, that is, the
+			 * value of the edge which reachs it.
+			 * 
+			 * In practical terms, we are always seeking, the path which takes from a node to another within the earliest possible time,
+			 * which means that, one node will not necessarily send a message through the first edge it connects to, because if it waits
+			 * for an opportunity to connect with a more suitable node, and edge, the message may arrive eariler to the destination node.
+			 * 
+			 * In our workaround, we always take into consideration, for the weight calculation, the  lower bound of the edge's time
+			 * intervals, because the transmission speed is always big enough to ensure the message will be successfully transmitted to
+			 * another node BEFORE the pair of nodes disconnect, that is, reaching a time greater than the connection interval's
+			 * upper bound.
+			 * 
+			 * */
+			if (getNodeTime(target) > getEdgeTime(node, target) /*+ getNodeWeight(node)*/) {
+				accumulated_time.put(target, getEdgeTime(node, target));
+				predecessors.put(target, node);
+				alternate_predecessors.put(target, node);
+				unvisitedNodes.add(target);
+			}
+		}
+	}
+	
+	// Returns the weight of a specific edge (receiving two end nodes as input)
+	public double getEdgeTime(Node node, Node target) {
+		for (Edge edge : edges) {
+			if (edge.getSource().equals(node) && edge.getDestination().equals(target)) {
+				return edge.getTime();
+			}else if (edge.getDestination().equals(node) && edge.getSource().equals(target)) {
+				return edge.getTime();
+			}
+		}
+		throw new RuntimeException("Should not happen");
+	}
+	
+	public List<Node> getReachableNeighbors(Node node) {
+		List<Node> neighbors = new ArrayList<Node>();
+		for (Edge edge : edges) {
+			/*
+			 * This mcgyverism here, is considering always the EARLIEST possible interval start times of each edge, so that one could
+			 * hope to reach the nodes as earliest as possible. This idea is because, if a pair of nodes are to reconnect, it means
+			 * a long time may have passed.
+			 * 
+			 * We compare the distance accumulated so far in a node with the edge's original earliest interval starting time.
+			 * If the accumulated distance is greater than said starting time, it means that, during the simulation, you would never
+			 * be able to reach said node, since the edge connection both nodes already disconnected (The pair of nodes may or
+			 * may not reconnect in the future.
+			 * 
+			 * Basically this process here IGNORES edges with weight smaller than the current origin node's weight (Since, considering
+			 * the simulation's timeline, if the node's weight is bigger than the edge's weight, this means, that the node can no longer
+			 * send packets through this edge).
+			 * */
+			if (edge.getTime() > accumulated_time.get(node)){
+				if (edge.getSource().equals(node) && !isVisited(edge.getDestination())) {
+					neighbors.add(edge.getDestination());
+					filtered_edges.add(edge);
+				}else if (edge.getDestination().equals(node) && !isVisited(edge.getSource())) {
+					neighbors.add(edge.getSource());
+					filtered_edges.add(edge);
+				}
+			}
+		}
+		return neighbors;
+	}
+	
+	// Returns the node with the smallest accumulated weight (distance) on it
+	/* Receives a node lists and search within it comparing the accumulated weights of each node */
+	public Node getMinimumTimeNode(Set<Node> nodes) {
+		Node minimum = null;
+		for (Node node : nodes) {
+			/*
+			 * Initially sets the first node it finds as the minimum, then it will compare the other nodes accumulated_weights, trying
+			 * to find the node with the least accumulated weight (distance)
+			 * */
+			if (minimum == null) {
+				minimum = node;
+			} else {
+				if (getNodeTime(node) < getNodeTime(minimum)) {
+					minimum = node;
+				}
+			}
+		}
+		return minimum;
+	}
+	
+	public boolean isVisited(Node vertex) {
+		return visitedNodes.contains(vertex);
+	}
+	
+	// Returns the accumulated time of the node input as argument
+	public double getNodeTime(Node destination) {
+		Double d = accumulated_time.get(destination);
+		if (d == null) {
+			return Double.POSITIVE_INFINITY; // Here is the famous process of setting everyone's weight, but source's weight (which is 0), as INFINITY.
+		} else {
+			return d;
+		}
+	}
+	
+	
+	// filtered edges search
+	
+	
+	/* getPath */
+	// This method returns the path from the source to the selected target and NULL if no path exists
+	public LinkedList<Node> getPath(Node target) {
+		LinkedList<Node> path = new LinkedList<Node>();
+		LinkedList<Node> alternate_path = new LinkedList<Node>();
+		Node step = target;
+		Node alternate_step = target;
+		// check if a path exists
+		if (predecessors.get(step) == null) {
+			return null;
+		}
+		
+		if (alternate_predecessors.get(step) == null) {
+			return null;
+		}
+		
+		path.add(step);// Adds destination node to the path (may lead to earliest possible route)
+		alternate_path.add(alternate_step); // Adds destination node to the alternate path (if above doesnt lead, this one may lead)
+		
+		while (predecessors.get(step) != null) {
+			step = predecessors.get(step);
+				path.add(step);
+		}
+		
+		while (alternate_predecessors.get(alternate_step) != null) {
+			alternate_step = alternate_predecessors.get(alternate_step);
+			if (this.getNodeTime(alternate_path.getLast()) > this.getNodeTime(alternate_step))
+				alternate_path.add(alternate_step);
+		}
+		
+		// Put it into the correct order
+		Collections.reverse(path);
+		Collections.reverse(alternate_path);
+
+		/* 
+		 * Ultimate mcgyverism warning: 
+		 * Had to do this to ensure in all situations (I hope) the arrival time at destination will be the earlist time possible!
+		 * 
+		 * It will compare the last edge of two paths, the edge with the smallest weight means the edge that should take me to the
+		 * destination node as fast as possible!
+		 * */
+		double final_time = 0.0;
+		double alternate_final_time = 0.0;
+		String aux_edge_label = path.get(path.size()-1).getLabel()+"--"+path.get(path.size()-2).getLabel();
+		Edge aux_edge = TVG.getInstance().getEdgeByLabel(aux_edge_label, filtered_edges);
+		if(aux_edge == null ){
+			aux_edge_label = path.get(path.size()-2).getLabel()+"--"+path.get(path.size()-1).getLabel();
+			aux_edge = TVG.getInstance().getEdgeByLabel(aux_edge_label, filtered_edges);			
+		}
+		final_time = aux_edge.getTime();
+		
+		aux_edge_label = alternate_path.get(alternate_path.size()-1).getLabel()+"--"+alternate_path.get(alternate_path.size()-2).getLabel();
+		aux_edge = TVG.getInstance().getEdgeByLabel(aux_edge_label, filtered_edges);
+		if(aux_edge == null ){
+			aux_edge_label = alternate_path.get(alternate_path.size()-2).getLabel()+"--"+alternate_path.get(alternate_path.size()-1).getLabel();
+			aux_edge = TVG.getInstance().getEdgeByLabel(aux_edge_label, filtered_edges);			
+		}
+		alternate_final_time = aux_edge.getTime();
+		//
+		// Depending on which of the generated paths contains the last edge with least edge, choose to return the path containing it!
+		if (final_time < alternate_final_time)
+			return path;
+		else
+			return alternate_path;
+	}
+}
